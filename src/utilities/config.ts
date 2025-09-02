db7c30459c1e2d066437c27ff4a4c6c0f3e4f38d
Tonyrealzy <umehobiarinze2@gmail.com>
2025-09-02 22:29:22 +0100

    chore: Changed the node version in my Docker file.

--- src/utilities/config.ts
+++ src/utilities/config.ts
@@ -8,7 +8,7 @@ export const envConfig = {
   limitRequest: process.env.LIMIT_REQUEST || 50, // limit each IP to 50 requests per windowMs,
   environment: process.env.NODE_ENV || "production",
   postgresUrl: process.env.DATABASE_URL,
-  baseUrl: process.env.BASE_URL,
+  baseUrl: process.env.BASE_URL || "",
   redisUrl: process.env.REDIS_URL,
   redisToken: process.env.REDIS_TOKEN,
 };
